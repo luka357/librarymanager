63a1c7b846b18a87ca08e7877a840b20e5abb240
luka357 <julayidzeluka@gmail.com>
2026-06-27 02:15:15 +0400

    added login and permissions

--- books/views.py
+++ books/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Book
 from .forms import BookForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
 
+@login_required
 def book_detail(request, pk):
     book = Book.objects.get(pk=pk)
     return render(request, 'books/book_detail.html', {'book': book})
 
+@login_required
 def book_add(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -21,6 +25,7 @@ def book_add(request):
         form = BookForm()
     return render(request, 'books/book_add.html', {'form': form})
 
+@login_required
 def book_delete(request, pk):
     book = Book.objects.get(pk=pk)
     if request.method == 'POST':
@@ -28,6 +33,7 @@ def book_delete(request, pk):
         return redirect('book_list')
     return render(request, 'books/book_delete.html', {'book': book})
 
+@login_required
 def book_update(request, pk):
     book = Book.objects.get(pk=pk)
     if request.method == 'POST':
--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('authors/', include('authors.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
